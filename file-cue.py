fb3a03dda32085c654f223caaffc02462434130f
4thkiindtech <151380870+4thkiindtech@users.noreply.github.com>
2023-11-21 15:51:04 -0500

    Add files via upload

---
+++ file-cue.py
@@ -0,0 +1,52 @@
+from flask import Flask, request, redirect, url_for, abort
+from werkzeug.utils import secure_filename
+import os
+
+app = Flask(__name__)
+
+# Configure the path to save the uploaded files
+UPLOAD_FOLDER = '/path/to/your/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Set the allowed file types
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'mp4', 'mov', 'wav', 'mp3'}
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # Check if the POST request has the file part
+        if 'file' not in request.files:
+            return redirect(request.url)
+
+        file = request.files['file']
+
+        # If the user does not select a file, the browser submits an empty file
+        # without a filename.
+        if file.filename == '':
+            return redirect(request.url)
+
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file', filename=filename))
+
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+if __name__ == '__main__':
+    app.run()
